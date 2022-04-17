d9baf5734abfa676b274ffbd1effc7dc889627d2
nghiaxd <93426925+nghiaxd@users.noreply.github.com>
2022-04-17 20:35:47 +0700

    Delete src directory

--- src/main/java/com/example/bloggle/repositories/ChuDeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.bloggle.repositories;
-
-import com.example.bloggle.entities.ChuDe;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChuDeRepository extends JpaRepository<ChuDe, Long>{
-    
-}
--- src/main/java/com/example/bloggle/serviceImpl/DanhGiaImpl.java
+++
@@ -1,6 +0,0 @@
-package com.example.bloggle.serviceImpl;
-
-
-public class DanhGiaImpl {
-    
-}
